0d54763c369bc6de198c4a00b3b1a01dc991cbf8
Jeff Hagkull <jeff@hagkull.com>
2014-05-15 23:33:00 -0500

    Getting masonry working, testing

--- js/app.js
+++ js/app.js
@@ -5,14 +5,45 @@ $(document).foundation();
 // Masonry
 
 
-var $containter = $('#container');
-$containter.imagesLoaded( function(){
-	$containter.masonry({
-		itemSelector: '.box',
-		isAnimated: !Modernizr.csstransitions,
-		isFitWidth: true
-	});	
+// var $container = $('#container');
+// $container.imagesLoaded( function(){
+// 	$container.masonry({
+// 		itemSelector: '.box',
+// 		isAnimated: !Modernizr.csstransitions,
+// 		isFitWidth: true
+// 	});	
+// });
+
+$( function() {
+
+  var $container = $('#container').masonry({
+    itemSelector: '.box',
+    columnWidth: 190
+  });
+    
+  // reveal initial images
+  $container.masonryImagesReveal( $('#images').find('.box') );
 });
+
+$.fn.masonryImagesReveal = function( $items ) {
+  var msnry = this.data('masonry');
+  var itemSelector = msnry.options.itemSelector;
+  // hide by default
+  $items.hide();
+  // append to container
+  this.append( $items );
+  $items.imagesLoaded().progress( function( imgLoad, image ) {
+    // get item
+    // image is imagesLoaded class, not <img>, <img> is image.img
+    var $item = $( image.img ).parents( itemSelector );
+    // un-hide item
+   $item.show();
+    // masonry does its thing
+    msnry.appended( $item );
+  });
+  
+  return this;
+};
 
 
 // AJAX call to display portfolio page and change colors
@@ -42,56 +73,22 @@ var assignColor = function(newCol) {
 	$( "#container" ).addClass(newCol);
 };
 
-// RUAN
-$( "#ruan" ).click(function() {
-	newColor = "ruan-color";
-	$( "#portfolio" ).load("ruan.html #portfolio > *");
-	if (color(document.body) != "ruan-color") {
+
+// Function that loads the correct portfolio page based on a click event
+// Remember: new colors and portfolio pages must in the form
+// page and page-color
+
+$('.book').click(function(){
+    var id = this.id;
+	var newColor = id + "-color";
+	$( "#portfolio" ).load(id + ".html", function(){
+		$(document).foundation('orbit').init;
+	});
+	if (color(document.body) != newColor) {
 		assignColor(newColor);
         $(window).scrollTop(0);
 	}
     else {
         $(window).scrollTop(0);
     }
-});
-
-// ISU
-$( "#isu" ).click(function() {
-    newColor = "isu-color";
-	$( "#portfolio" ).load("isu.html #portfolio > *");
-	if (color(document.body) != "isu-color") {
-		assignColor(newColor);
-        $(window).scrollTop(0);
-	}
-    else {
-        $(window).scrollTop(0);
-    }
-});
-	
-
-//Energyficient
-$( "#energyficient" ).click(function() {
-	newColor = "energy-color";
-	$( "#portfolio" ).load("energy.html #portfolio > *");
-	if (color(document.body) != "energy-color") {
-		assignColor(newColor);
-        $(window).scrollTop(0);
-	}
-    else {
-        $(window).scrollTop(0);
-    }
-});
-
-// DCA
-$( "#dca" ).click(function() {
-	newColor = "dca-color";
-	$( "#portfolio" ).load("dca.html #portfolio > *");
-	if (color(document.body) != "dca-color") {
-		assignColor(newColor);
-        $(window).scrollTop(0);
-	}
-    else {
-        $(window).scrollTop(0);
-    }
-});
-
+}); 
