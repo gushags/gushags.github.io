7383025a9e1154b980c8df3db700c1118ca9963f
Jeff Hagkull <jeff@hagkull.com>
2014-10-17 19:27:42 -0700

    Yet another app.js update

--- js/app.js
+++ js/app.js
@@ -200,25 +200,23 @@ $(document).foundation();
 })(jQuery, document); // document ready end
 
 // StickyNavbar code
-$(function () {
-    $('.header').stickyNavbar({
-    activeClass: "active",          // Class to be added to highlight nav elements
-    //sectionSelector: "scrollto",   // Class of the section that is interconnected with nav links
-    //animDuration: 1100,              // Duration of jQuery animation
-    startAt: 0,                     // Stick the menu at XXXpx from the top of the this() (nav container)
-    //easing: "jswing",               // Easing type if jqueryEffects = true, use jQuery Easing plugin to extend easing types - gsgd.co.uk/sandbox/jquery/easing
-    animateCSS: false,               // AnimateCSS effect on/off
-    animateCSSRepeat: false,        // Repeat animation everytime user scrolls
-    cssAnimation: "fadeInDown",     // AnimateCSS class that will be added to selector
-    // jqueryEffects: true,           // jQuery animation on/off
-    //jqueryAnim: "slideDown",        // jQuery animation type: fadeIn, show or slideDown
-    selector: "a",                  // Selector to which activeClass will be added, either "a" or "li"
-    mobile: false,                  // If false nav will not stick under 480px width of window
-    mobileWidth: 1024,               // The viewport width (without scrollbar) under which stickyNavbar will not be applied (due usability on mobile devices)
-    zindex: 9999,                   // The zindex value to apply to the element: default 9999, other option is "auto"
-    stickyModeClass: "sticky",      // Class that will be applied to 'this' in sticky mode
-    unstickyModeClass: "unsticky"   // Class that will be applied to 'this' in non-sticky mode
-  });
+$('.header').stickyNavbar({
+	activeClass: "active",          // Class to be added to highlight nav elements
+	//sectionSelector: "scrollto",   // Class of the section that is interconnected with nav links
+	//animDuration: 1100,              // Duration of jQuery animation
+	startAt: 0,                     // Stick the menu at XXXpx from the top of the this() (nav container)
+	//easing: "jswing",               // Easing type if jqueryEffects = true, use jQuery Easing plugin to extend easing types - gsgd.co.uk/sandbox/jquery/easing
+	animateCSS: false,               // AnimateCSS effect on/off
+	animateCSSRepeat: false,        // Repeat animation everytime user scrolls
+	cssAnimation: "fadeInDown",     // AnimateCSS class that will be added to selector
+	// jqueryEffects: true,           // jQuery animation on/off
+	//jqueryAnim: "slideDown",        // jQuery animation type: fadeIn, show or slideDown
+	selector: "a",                  // Selector to which activeClass will be added, either "a" or "li"
+	mobile: false,                  // If false nav will not stick under 480px width of window
+	mobileWidth: 1024,               // The viewport width (without scrollbar) under which stickyNavbar will not be applied (due usability on mobile devices)
+	zindex: 9999,                   // The zindex value to apply to the element: default 9999, other option is "auto"
+	stickyModeClass: "sticky",      // Class that will be applied to 'this' in sticky mode
+	unstickyModeClass: "unsticky"   // Class that will be applied to 'this' in non-sticky mode
 });
 
 
